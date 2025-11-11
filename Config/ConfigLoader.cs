268840c4cb8adc1a9a1d3eee1a4041a76c054d53
Otavio <roqueotavio5@gmail.com>
2025-11-11 15:21:50 -0300

    Criei a classe ConfigLoader pra carregar as connectionstrings

---
+++ Config/ConfigLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace FastRestorer.Config
+{
+    public static class ConfigLoader
+    {
+        public static string GetConnectionString()
+        {
+            string localConfigPath = "Config/App.Local.config";
+            string connectionName = "DefaultConnection";
+
+            if (!File.Exists(localConfigPath))
+            {
+                string msg = $"O arquivo de configuração local '{localConfigPath}' não foi encontrado." +
+                    Environment.NewLine + "Leia o README.md da pasta Config para mais informações.";
+
+                throw new FileNotFoundException(msg);
+            }
+
+            var map = new ExeConfigurationFileMap { ExeConfigFilename = localConfigPath };
+            var config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+            var connectionString = config.ConnectionStrings.ConnectionStrings[connectionName];
+
+            if (connectionString == null)
+            {
+                string msg = $"A connection string '{connectionName}' não foi encontrada no arquivo '{localConfigPath}'." +
+                    Environment.NewLine + $"Certifique-se que App.Local.config tenha o name definido como '{connectionName}'.";
+
+                throw new ConfigurationErrorsException(msg);
+            }
+
+            return connectionString.ConnectionString;
+        }
+    }
+}
