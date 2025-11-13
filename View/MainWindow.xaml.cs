74ed044678ad442a628d6fbee0fcffe8c6b5edea
Otavio <roqueotavio5@gmail.com>
2025-11-13 18:11:33 -0300

    Criei os eventos de drop e click pra selecionar os arquivos

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,24 +1,26 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FastRestorer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Events
+
+        private void GridDropArea_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void BtnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 }
