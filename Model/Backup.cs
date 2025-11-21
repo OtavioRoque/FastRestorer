f64ab9048ff50f66551c718f483ab21c87b1d53d
Otavio <roqueotavio5@gmail.com>
2025-11-21 09:06:29 -0300

    feat: creation of the Backup class in the new folder Model

---
+++ Model/Backup.cs
@@ -0,0 +1,18 @@
+﻿namespace FastRestorer.Model
+{
+    public class Backup
+    {
+        private readonly long _sizeBytes;
+
+        public string DatabaseName { get; }
+        public DateTime BackupDate { get; }
+        public double SizeMb => _sizeBytes / 1024d / 1024d;
+
+        public Backup(string databaseName, DateTime backupDate, long sizeBytes)
+        {
+            DatabaseName = databaseName;
+            BackupDate = backupDate;
+            _sizeBytes = sizeBytes;
+        }
+    }
+}
